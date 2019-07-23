38c2baac2a935653746c6207b22ffbf47b32cdc1
tpetersen0308 <travis.e.petersen@gmail.com>
2019-07-23 15:41:31 -0500

    [WIP] Restart oauth configuration process

--- lib/rebay_api_web/router.ex
+++ lib/rebay_api_web/router.ex
@@ -14,14 +14,6 @@ defmodule RebayApiWeb.Router do
     plug :accepts, ["json"]
   end
 
-  pipeline :auth do
-    plug :accepts, ["json"]
-    plug :fetch_session
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-    plug RebayApiWeb.Plugs.SetUser
-  end
-
   scope "/api", RebayApiWeb do
     pipe_through :api
     resources "/items", ItemController, except: [:new, :edit]
