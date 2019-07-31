a9d60880c87520328ba67b8b44a49cb43c384e4f
serpient <littlegangwolf@gmail.com>
2019-07-31 10:53:18 -0700

    Remove unused user routes and controllers

--- test/rebay_api_web/controllers/user_controller_test.exs
+++ test/rebay_api_web/controllers/user_controller_test.exs
@@ -10,13 +10,6 @@ defmodule RebayApiWeb.UserControllerTest do
     provider: "some provider",
     uuid: "7488a646-e31f-11e4-aace-600308960662"
   }
-  @update_attrs %{
-    avatar: "some updated avatar",
-    email: "some updated email",
-    first_name: "some updated first_name",
-    provider: "some updated provider",
-    uuid: "7488a646-e31f-11e4-aace-600308960662"
-  }
   @invalid_attrs %{avatar: nil, email: nil, first_name: nil, provider: nil, uuid: nil}
 
   def fixture(:user) do
@@ -55,48 +48,5 @@ defmodule RebayApiWeb.UserControllerTest do
       conn = post(conn, Routes.user_path(conn, :create), user: @invalid_attrs)
       assert json_response(conn, 422)["errors"] != %{}
     end
-  end
-
-  describe "update user" do
-    setup [:create_user]
-    test "renders user when data is valid", %{conn: conn, user: user} do
-      uuid = user.uuid
-      conn = put(conn, Routes.user_path(conn, :update, uuid), user: @update_attrs)
-
-      assert %{"uuid" => ^uuid} = json_response(conn, 200)["data"]
-
-      conn = get(conn, Routes.user_path(conn, :show, uuid))
-
-      assert %{
-               "avatar" => "some updated avatar",
-               "email" => "some updated email",
-               "first_name" => "some updated first_name",
-               "provider" => "some updated provider",
-               "uuid" => "7488a646-e31f-11e4-aace-600308960662"
-              } = json_response(conn, 200)["data"]
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, user: user} do
-      conn = put(conn, Routes.user_path(conn, :update, user.uuid), user: @invalid_attrs)
-      assert json_response(conn, 422)["errors"] != %{}
-    end
-  end
-
-  describe "delete user" do
-    setup [:create_user]
-
-    test "deletes chosen user", %{conn: conn, user: user} do
-      conn = delete(conn, Routes.user_path(conn, :delete, user.uuid))
-      assert response(conn, 204)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.user_path(conn, :show, user.uuid))
-      end
-    end
-  end
-
-  defp create_user(_) do
-    user = fixture(:user)
-    {:ok, user: user}
   end
 end
